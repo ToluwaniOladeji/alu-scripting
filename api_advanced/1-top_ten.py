1c4a1cae3e9620d058144680f0ecf46121e3bd54
ToluwaniOladeji <t.oladeji@alustudent.com>
2025-10-26 08:03:41 +0000

    1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -7,7 +7,7 @@ def top_ten(subreddit):
     """Docs"""
     reddit_url = "https://www.reddit.com/r/{}/hot.json" \
         .format(subreddit)
-    headers = headers = {'User-agent': 'Tolu/5.0'}
+    headers = headers = {'User-agent': 'PostmanRuntime/7.35.0'}
     response = requests.get(reddit_url, headers=headers)
 
     if response.status_code == 200:
